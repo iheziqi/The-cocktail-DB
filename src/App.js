988cef76dfec069cfb9bd22f0403478d76763749
Ziqi He <52439236+iheziqi@users.noreply.github.com>
2023-03-19 11:39:06 +0100

    setup context api

--- src/App.js
+++ src/App.js
@@ -13,14 +13,13 @@ import Navbar from './components/Navbar';
 function App() {
 	return (
 		<Router>
-			<Navbar>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="about" element={<About />} />
-					<Route path="cocktail/:id" element={<SingleCocktail />} />
-					<Route path="*" element={<Error />} />
-				</Routes>
-			</Navbar>
+			<Navbar></Navbar>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="about" element={<About />} />
+				<Route path="cocktail/:id" element={<SingleCocktail />} />
+				<Route path="*" element={<Error />} />
+			</Routes>
 		</Router>
 	);
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
-  return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+	return (
+		<section className="section about-section">
+			<h1 className="section=title">about us</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi eos
+				praesentium natus deleniti deserunt ducimus porro ex assumenda. Quae,
+				commodi ea? Necessitatibus pariatur esse deserunt asperiores similique!
+				Deserunt molestiae doloremque expedita beatae accusamus ad maiores,
+				blanditiis labore esse, earum mollitia?
+			</p>
+		</section>
+	);
+};
 
-export default About
+export default About;
